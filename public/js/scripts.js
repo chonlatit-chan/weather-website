44cc70d8e49fbfa13041b5478303675fe113ac1d
Mayjung-Legion5 <chonlatit@dek-d.com>
2021-06-07 15:54:54 +0700

    Setup app for heroku

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -16,7 +16,7 @@ weatherForm.addEventListener('submit', (e) => {
 
     const address = search.value
 
-    fetch('http://localhost:3000/weather?address=' + encodeURIComponent(address)).then((response) => {
+    fetch('/weather?address=' + encodeURIComponent(address)).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error
